02368fcb0c56f8cc33c2347e15b32504484055c1
Outwerk <outows50@gmail.com>
2024-10-07 07:14:07 -0700

    make more responsive

--- src/pages/HeroSection.js
+++ src/pages/HeroSection.js
@@ -5,12 +5,12 @@ import HeroForm from '../components/HeroForm';
 export default function HeroSection() {
     return (
         < >
-            <div className='w-full flex justify-center sm:justify-start  md:justify-evenly items-center  flex-wrap md:flex-nowrap mt-[-38px] md:mt-[-20px] mb-10 md:mx-20 '>
+            <div className='w-full flex justify-center sm:justify-start  md:justify-evenly items-center  flex-wrap md:flex-nowrap mt-[-38px] md:mt-[-20px] mb-10  md:mx-20 '>
 
                 {/* Div for Content */}
-                <div className='flex justify-center md:justify-start items-center self-center md:self-start flex-col gap-5 mt-[-50px] md:mt-0 p-3 md:p-0 sm:ms-6 ms-0'>
+                <div className='flex justify-center md:justify-start items-center self-center md:self-start flex-col gap-5 mt-[-50px] md:mt-0 p-3 md:p-0 sm:ms-6 ms-2'>
                     {/* Bar On heading */}
-                    <div className='self-start ms-2 min-h-4 mt-32 min-w-80 md:min-w-96 bar rounded-full'></div>
+                    <div className='self-start sm:ms-2 ms-1 min-h-4 mt-32 min-w-80 md:min-w-96 bar rounded-full'></div>
 
                     <div className='self-start md:self-center me-3 md:me-0 '>
                         <h1 className='text-4xl sm:text-[80px] leading-none font-semibold'>
@@ -30,7 +30,7 @@ export default function HeroSection() {
                 </div>
 
                 {/* Div for Form */}
-                <div className='md:mt-16 mt-0 '>
+                <div className='md:mt-16 mt-0 sm:ms-0 ms-[-10px]'>
                     <HeroForm />
                 </div>
 
