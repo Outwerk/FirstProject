a2a7e79846f3a96e85f523236bff14e79c9b0cd7
Outwerk <outows50@gmail.com>
2024-10-09 07:16:48 -0700

    add logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import { FaPhone} from "react-icons/fa";
-import Logo from "../assets/LOGO.jpg"
+import Logo from "../assets/LOGO.png"
 
 
 
 export default function Navbar() {
   return (
     <>
-      <div className='flex md:justify-center justify-between md:px-0 px-6 items-center gap-10 w-full h-20 md:my-3'>
-        <div className='cursor-pointer'><img src={Logo} alt='LOGO' className='w-28 h-20 me-[-50px] z-50' /></div>
+      <div className='flex md:justify-center justify-between md:px-0 px-6 items-center gap-7 w-full h-20 md:my-3'>
+        <div className='cursor-pointer'><img src={Logo} alt='LOGO' className='w-32 h-14 me-[-50px] z-50' /></div>
         <div className=''>
           <a href="#" className="hover:bg-[#04200E]"><h2 className='bg-[#006738] text-[#FFFFFF] md:p-4 md:px-8 px-4 py-3 rounded-md text-sm font-bold hover:bg-[#04200E] transition-all duration-300 whitespace-nowrap'>Get Started</h2></a>
         </div>
